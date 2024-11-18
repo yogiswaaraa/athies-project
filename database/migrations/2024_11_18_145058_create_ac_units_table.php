c201a2ba75d5928824d4deb3ae419f9b863768f7
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-11-18 22:04:43 +0700

    add migration

---
+++ database/migrations/2024_11_18_145058_create_ac_units_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ac_units', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('building_id')->constrained()->onDelete('cascade');
+            $table->string('unit_code')->unique();
+            $table->string('model');
+            $table->string('serial_number');
+            $table->enum('status', ['active', 'maintenance', 'inactive']);
+            $table->float('current_temperature')->nullable();
+            $table->float('efficiency_rating')->nullable();
+            $table->date('installation_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ac_units');
+    }
+};
---
+++ database/migrations/2024_11_18_145540_create_maintenance_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenance_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ac_unit_id')->constrained()->onDelete('cascade');
+            $table->date('maintenance_date');
+            $table->string('technician_name');
+            $table->text('actions_taken'); // todo: create enum or table for this
+            $table->text('notes')->nullable();
+            $table->enum('result', ['success', 'partial', 'failed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenance_histories');
+    }
+};
