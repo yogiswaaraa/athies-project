ae5886366610630b4c248278ba61ad75473bc22c
lychrtee <lychrteee02@gmail.com>
2024-11-19 17:16:24 +0700

    nama gedung

--- app/Filament/Resources/BuildingResource/Pages/ListBuildings.php
+++ app/Filament/Resources/BuildingResource/Pages/ListBuildings.php
@@ -9,7 +9,7 @@ use Filament\Resources\Pages\ListRecords;
 class ListBuildings extends ListRecords
 {
     protected static string $resource = BuildingResource::class;
-    protected ?string $heading = 'Daftar Gedung';
+    protected ?string $heading = 'Nama Gedung';
 
     protected function getHeaderActions(): array
     {
