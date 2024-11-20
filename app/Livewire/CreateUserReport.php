44409bcc5ef9e17043b41838cb20c33b3444fbe5
Hestri074 <20523074@students.uii.ac.id>
2024-11-20 17:00:58 +0700

    livewire form init

---
+++ app/Livewire/CreateUserReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreateUserReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-user-report');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\CreateUserReport;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/UserReport', CreateUserReport::class);
