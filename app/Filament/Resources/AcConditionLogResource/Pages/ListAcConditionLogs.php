31847fb67b79170bef2ef19089007f4090ff4178
yuanda@yoru <34027873+yuandahanif@users.noreply.github.com>
2024-11-21 15:03:25 +0700

    improve publist test message

--- app/Filament/Resources/AcConditionLogResource/Pages/ListAcConditionLogs.php
+++ app/Filament/Resources/AcConditionLogResource/Pages/ListAcConditionLogs.php
@@ -3,17 +3,40 @@
 namespace App\Filament\Resources\AcConditionLogResource\Pages;
 
 use App\Filament\Resources\AcConditionLogResource;
-use App\Services\RabbitMQService;
-use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Forms;
+use Illuminate\Contracts\View\View;
+use App\Services\RabbitMQService;
+
 
 class ListAcConditionLogs extends ListRecords
 {
     protected static string $resource = AcConditionLogResource::class;
 
     protected ?string $heading = 'Riwayat Kondisi Unit AC';
+
+
+    public function publishMessage(): void
+    {
+        $mqService = new RabbitMQService();
+        $dummy = [
+            'temperature' => fake()->randomFloat(2, 20, 30),
+            'ac_unit_id' => fake()->randomNumber(1, 10),
+            'humidity' => fake()->randomFloat(2, 40, 60),
+            'power_consumption' => fake()->randomNumber(1, 10),
+        ];
+
+
+        $mqService->publish(json_encode($dummy));
+    }
+
+    public function publishNTimes(int $n): void
+    {
+        for ($i = 0; $i < $n; $i++) {
+            $this->publishMessage();
+        }
+    }
+
     protected function getHeaderActions(): array
     {
         return [
@@ -21,26 +44,9 @@ class ListAcConditionLogs extends ListRecords
 
             Action::make('test_mqtt')
                 ->label('Test MQTT')
-                ->form([
-                    // AcConditionLogResource::schema()
-                    Forms\Components\TextInput::make('temperature')
-
-                ])
-                ->action(function (array $data): void {
-                    $dummy = [
-                        'temperature' => 10,
-                        'ac_unit_id' => 1,
-                        'humidity' => 10,
-                        'power_consumption' => 10
-                    ];
-
-                    $mqService = new RabbitMQService();
-
-
-                    $mqService->publish(json_encode($dummy));
-
-                    return;
-                })
+                ->modalContent(fn(): View => view(
+                    'livewire.mqtt-publish-test',
+                ))->modalSubmitAction(false)
         ];
     }
 }
---
+++ resources/views/livewire/mqtt-publish-test.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <div class="flex justify-center items-center gap-2">
+        <button wire:click="publishMessage" class="">publish</button>
+        <button wire:click="publishNTimes(10)" class="">publish 10x</button>
+    </div>
+</div>
